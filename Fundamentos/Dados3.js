0c17c36686732797f2d7637e516ca54295d12ed7
Thainá Justo <126414871+Thainajusto@users.noreply.github.com>
2023-03-03 19:21:59 -0300

    Add files via upload

---
+++ Fundamentos/Dados3.js
@@ -0,0 +1,11 @@
+let idade = 27;
+console.log(typeof 27);
+console.log (typeof idade);
+
+let salario = 4578.32
+console.log(typeof salario)
+
+console.log(typeof "Teste");
+
+let estaChovendo = true // ou fase
+console.log(typeof estaChovendo);
---
+++ Fundamentos/Dados_troca.js
@@ -0,0 +1,15 @@
+let a = 7;
+let b = 94;
+
+// depois da troca a = 94 e b = 7
+
+console.log(a);
+console.log(b);
+
+
+console.log(a = 94);
+console.log(b = 7);
+
+let temp = a;
+a = b
+b = temp
